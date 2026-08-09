ad135939250066148e77d2e76e9c1f2b72a51097
BG-0o <1koBG.0o@gmail.com>
2026-08-09 07:30:42 -0400

    Create ToxHud.lua

---
+++ ToxHud.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SEU_USUARIO/SEU_REPO/main/Interface.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SEU_USUARIO/SEU_REPO/main/Functions.lua"))()
+print("hi");
